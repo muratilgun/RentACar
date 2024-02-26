b73ad3aa61ffdd185af1ec13af444037d5be6b69
Murat Ilgun <muratilgun34@gmail.com>
2024-02-27 00:47:07 +0300

    Added brand en

---
+++ RentACar.Domain/Entities/Brand.cs
@@ -0,0 +1,15 @@
+﻿using Core.Persistence.Repositories;
+
+namespace RentACar.Domain.Entities;
+
+public class Brand : Entity<Guid>
+{
+    public string Name { get; set; }
+    public Brand(){}
+    public Brand(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+}
