4399c1d2a5fe7a90a37bc7428e3253230d04bd4a
Murat Ilgun <muratilgun34@gmail.com>
2024-03-02 22:00:20 +0300

    Added query

--- RentACar.Api/Controllers/BrandsController.cs
+++ RentACar.Api/Controllers/BrandsController.cs
@@ -1,6 +1,9 @@
-﻿using MediatR;
+﻿using Core.Application.Requests;
+using Core.Application.Responses;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using RentACar.Application.Features.Brands.Commands.Create;
+using RentACar.Application.Features.Brands.Queries.GetList;
 
 namespace RentACar.Api.Controllers;
 [Route("api/[controller]")]
@@ -11,6 +14,14 @@ public class BrandsController : BaseController
     public async Task<ActionResult<CreatedBrandResponse>> Create([FromBody] CreateBrandCommand command)
     {
         CreatedBrandResponse response = await Mediator.Send(command);
+        return Ok(response);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListBrandQuery query = new GetListBrandQuery { PageRequest = pageRequest };
+        GetListResponse<GetListBrandListItemDto> response = await Mediator.Send(query);
         return Ok(response);
     }
 }
--- RentACar.Application/Features/Brands/Profiles/MappingProfiles.cs
+++ RentACar.Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using RentACar.Domain.Entities;
 using RentACar.Application.Features.Brands.Commands.Create;
+using Core.Persistence.Paging;
+using Core.Application.Responses;
+using RentACar.Application.Features.Brands.Queries.GetList;
 
 namespace RentACar.Application.Features.Brands.Profiles;
 public class MappingProfiles : Profile
@@ -9,5 +12,7 @@ public class MappingProfiles : Profile
     {
         CreateMap<Brand, CreateBrandCommand>().ReverseMap();
         CreateMap<Brand, CreatedBrandResponse>().ReverseMap();
+        CreateMap<Brand, GetListBrandListItemDto>().ReverseMap();
+        CreateMap<Paginate<Brand>, GetListResponse<GetListBrandListItemDto>>().ReverseMap();
     }
 }
