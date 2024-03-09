bc4dbc89f513caa6dcd1ec77a41523e15c000d29
Murat Ilgun <muratilgun34@gmail.com>
2024-03-10 00:20:20 +0300

    #

--- RentACar.Api/Program.cs
+++ RentACar.Api/Program.cs
@@ -1,3 +1,4 @@
+using Core.CrossCuttingConcerns.Exceptions.Extensions;
 using RentACar.Application;
 using RentACar.Persistence;
 
@@ -24,6 +25,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+if (app.Environment.IsProduction())
+    app.ConfigureCustomExceptionMiddleware();
 
 app.UseHttpsRedirection();
 
