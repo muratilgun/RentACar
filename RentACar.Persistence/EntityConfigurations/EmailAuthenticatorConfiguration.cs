6cf44fae2ce1f7c24ce80e0ef2b393d1889cac43
Murat Ilgun <muratilgun34@gmail.com>
2024-03-14 00:02:03 +0300

    Added Email & Otp Config

---
+++ RentACar.Persistence/EntityConfigurations/EmailAuthenticatorConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace RentACar.Persistence.EntityConfigurations;
+public class EmailAuthenticatorConfiguration : IEntityTypeConfiguration<EmailAuthenticator>
+{
+    public void Configure(EntityTypeBuilder<EmailAuthenticator> builder)
+    {
+        builder.ToTable("EmailAuthenticators").HasKey(ea => ea.Id);
+
+        builder.Property(ea => ea.Id).HasColumnName("Id").IsRequired();
+        builder.Property(ea => ea.UserId).HasColumnName("UserId").IsRequired();
+        builder.Property(ea => ea.ActivationKey).HasColumnName("ActivationKey");
+        builder.Property(ea => ea.IsVerified).HasColumnName("IsVerified").IsRequired();
+        builder.Property(ea => ea.CreatedAt).HasColumnName("CreatedAt").IsRequired();
+        builder.Property(ea => ea.UpdatedAt).HasColumnName("UpdatedAt");
+        builder.Property(ea => ea.DeletedAt).HasColumnName("DeletedAt");
+
+        builder.HasQueryFilter(ea => !ea.DeletedAt.HasValue);
+
+        builder.HasOne(ea => ea.User);
+    }
+}
---
+++ RentACar.Persistence/EntityConfigurations/OtpAuthenticatorConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace RentACar.Persistence.EntityConfigurations;
+public class OtpAuthenticatorConfiguration : IEntityTypeConfiguration<OtpAuthenticator>
+{
+    public void Configure(EntityTypeBuilder<OtpAuthenticator> builder)
+    {
+        builder.ToTable("OtpAuthenticators").HasKey(oa => oa.Id);
+
+        builder.Property(oa => oa.Id).HasColumnName("Id").IsRequired();
+        builder.Property(oa => oa.UserId).HasColumnName("UserId").IsRequired();
+        builder.Property(oa => oa.Secret).HasColumnName("Secret").IsRequired();
+        builder.Property(oa => oa.IsVerified).HasColumnName("IsVerified").IsRequired();
+        builder.Property(oa => oa.CreatedAt).HasColumnName("CreatedAt").IsRequired();
+        builder.Property(oa => oa.UpdatedAt).HasColumnName("UpdatedAt");
+        builder.Property(oa => oa.DeletedAt).HasColumnName("DeletedAt");
+
+        builder.HasQueryFilter(oa => !oa.DeletedAt.HasValue);
+
+        builder.HasOne(oa => oa.User);
+    }
+}
