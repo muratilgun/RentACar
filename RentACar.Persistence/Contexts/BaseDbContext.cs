68974ace8b2d77c4bb4f21e848997480b729f460
Murat Ilgun <muratilgun34@gmail.com>
2024-03-13 23:40:10 +0300

    Added Security Entities

--- RentACar.Persistence/Contexts/BaseDbContext.cs
+++ RentACar.Persistence/Contexts/BaseDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using RentACar.Domain.Entities;
 using System.Reflection;
@@ -12,6 +13,13 @@ public class BaseDbContext : DbContext
     public DbSet<Model> Models { get; set; }
     public DbSet<Fuel> Fuels { get; set; }
     public DbSet<Transmission> Transmissions { get; set; }
+    public DbSet<OperationClaim> OperationClaims{ get; set; }
+    public DbSet<OtpAuthenticator> OtpAuthenticators{ get; set; }
+    public DbSet<RefreshToken> RefreshTokens{ get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+
 
     public BaseDbContext(DbContextOptions<BaseDbContext> options, IConfiguration configuration) : base(options)
     {
