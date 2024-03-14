cd00c90adb49cbdfef378e77bbf6ed97ab2cb4fa
Murat Ilgun <muratilgun34@gmail.com>
2024-03-14 18:36:43 +0300

    Added CreateUserCommand

---
+++ RentACar.Application/Features/Users/Profiles/MappingProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Core.Security.Entities;
+using RentACar.Application.Features.Users.Commands.Create;
+
+namespace RentACar.Application.Features.Users.Profiles;
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<User, CreateUserCommand>().ReverseMap();
+        CreateMap<User, CreatedUserResponse>().ReverseMap();
+    }
+}
